d49b6749d79c5dddc640feca2725f6e4a007d13d
JonGreaves2004 <163329476+JonGreaves2004@users.noreply.github.com>
2025-09-19 16:05:16 +0100

    Delete sanity directory

--- sanity/schemas/index.ts
+++
@@ -1,7 +0,0 @@
-import competition from './objects/competition'
-import ruleTopic from './objects/ruleTopic'
-import notice from './objects/notice'
-import faq from './objects/faq'
-import download from './objects/download'
-
-export default [competition, ruleTopic, notice, faq, download]
