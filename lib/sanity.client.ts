83c8874f1099a4f00d9739ea7aea8ceea3b388ef
JonGreaves2004 <163329476+JonGreaves2004@users.noreply.github.com>
2025-09-19 16:05:26 +0100

    Delete lib directory

--- lib/sanity.client.ts
+++
@@ -1,9 +0,0 @@
-import {createClient} from '@sanity/client'
-
-export const sanityClient = createClient({
-  projectId: process.env.SANITY_PROJECT_ID,
-  dataset: process.env.SANITY_DATASET,
-  apiVersion: process.env.SANITY_API_VERSION || '2024-06-01',
-  useCdn: true,
-  token: process.env.SANITY_READ_TOKEN
-})
